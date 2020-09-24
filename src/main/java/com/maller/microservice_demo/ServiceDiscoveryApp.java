abe68f4d37bcfb225ad1484b08c641d97ba427ed
Vineeth Maller <vineethmaller@gmail.com>
2020-09-24 21:45:34 +0530

    Initial commit

---
+++ src/main/java/com/maller/microservice_demo/ServiceDiscoveryApp.java
@@ -0,0 +1,15 @@
+package com.maller.microservice_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/maller/demo/microservice/MicroserviceServiceDiscoverryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maller.demo.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceServiceDiscoverryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
